93676f4e030c1c738b8d6195ddabcaddb930d7ae
James Bailey <jimi.bailey@gmail.com>
2013-10-24 12:34:52 +0100

    Updated README.md and the variable names in app/auth/index.js

--- py/upload.py
+++ py/upload.py
@@ -3,7 +3,7 @@
 from rauth import OAuth1Session
 import requests, hashlib, time, random
 
-useValidCredentials = True
+useValidCredentials = False
 useValidCheckSum = True
 useRandomHash = True
 useValidTimeStamp = True
